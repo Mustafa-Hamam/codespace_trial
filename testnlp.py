d5f1a05597a796c81a1219d58460d3564fe88842
Mustafa-Hamam <mostafa.hamam86@gmail.com>
2025-05-23 14:19:47 +0000

    adding fixed test with proper imports

--- testnlp.py
+++ testnlp.py
@@ -1,4 +1,4 @@
-from nlp_logic.nlp import get_wiki, sum_wiki, get_txtblob,get_phrases
+from nlp_logic.nlp import get_phrases
 
 def test_get_phrases():
     assert 'golden state' in get_phrases("Golden State Warriors")
